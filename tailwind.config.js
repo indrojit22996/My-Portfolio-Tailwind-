1c0ec36714dfd66b9945841f2782c33417b3a24f
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-06-17 20:21:30 +0530

    Add My Info

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./main/**/*.{html,js}',
-  './components/**/*.{html,js}'],
+  content: ['./main/*.html'],
   theme: {
     extend: {},
   },
